ef1e0f7d7ece368363d553518307da90ce6aec44
gambolkf <gambolkf@gmail.com>
2024-06-05 21:27:20 +0200

    test: added test file for easier testing

--- tkom/src/main/java/interpreter/Main.java
+++ tkom/src/main/java/interpreter/Main.java
@@ -1,27 +1,21 @@
 package interpreter;
 
 
+import inputHandle.FileSource;
 import inputHandle.Source;
-import inputHandle.StringSource;
-import interpreter.embedded.Value;
 import lekser.TokenBuilder;
 import parser.Parser;
 import parser.parsableObjects.Program;
 
-import java.util.LinkedHashMap;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Source src = new StringSource("print(10/0)");
+        Source src = new FileSource("src/test/generalTest.sp");
         TokenBuilder tokenBuilder = new TokenBuilder(src);
         Parser parser = new Parser(tokenBuilder);
         Program program = parser.parseProgram();
         Interpreter interpreter = new Interpreter();
         interpreter.interpret(program);
 
-    }
-
-    private static void setVariable(Value va) {
-        va.setValue(10);
     }
 }
